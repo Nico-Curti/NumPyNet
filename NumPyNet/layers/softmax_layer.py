89528d1920992dc1b105c0784e80d0335c6c0516
Mattia Ceccarelli <mattia.ceccarelli3@gmail.com>
2019-08-03 11:10:53 +0200

    fmt fixed

--- NumPyNet/layers/softmax_layer.py
+++ NumPyNet/layers/softmax_layer.py
@@ -10,11 +10,10 @@ __author__ = ['Mattia Ceccarelli', 'Nico Curti']
 __email__ = ['mattia.ceccarelli3@studio.unibo.it', 'nico.curti2@unibo.it']
 __package__ = "Softmax layer"
 
-
 class Softmax_layer():
 
   def __init__(self, groups = 1, spatial = False, temperature = 1.):
-    """
+    '''
     Softmax layer: perfoms a Softmax transformation of its input
 
     Parameters:
@@ -23,8 +22,9 @@ class Softmax_layer():
       spatial      : boolean, default is False. if True performs the softmax
         computing max and sum over the entire image. if False max and sum are computed over
         the last axes (channels)
-      temperature  : float, default is 1.. divide max and input in the softmax formulation.
-    """
+      temperature  : float, default is 1.. divide max and input in the softmax formulation
+        used only is spatial is False
+    '''
 
     self.batch, self.w,self.h,self.c = (0, 0, 0, 0)
     self.output, self.delta, self.loss  = (None,None,None)
@@ -44,54 +44,55 @@ class Softmax_layer():
 
 
   def forward(self, inpt, truth = None) :
-    """
+    '''
     Forward function of the Softmax Layer.
 
     Parameters:
       inpt  : array of shape (batch, w, h, c), input array
       truth : array of shape (batch, w, h, c), default is None, target vector.
         if a value is passed, the function compute the cross entropy cost
-    """
+    '''
 
     self.batch, self.w, self.h, self.c = inpt.shape
 
     if self.spatial:
-      self.output = np.exp(inpt - inpt.max(axis = -1, keepdims=True))
-      s = self.output.sum(axis = -1, keepdims = True)
+      self.output = np.exp(inpt - inpt.max(axis=-1, keepdims=True))
+      s = self.output.sum(axis=-1, keepdims=True)
 
-    else : #groups is still fixed to 1
+    else : # groups is still fixed to 1
       self.output = np.exp((inpt - np.max(inpt, axis=(1,2,3), keepdims=True))*self.temperature)
       s = self.output.sum(axis=(1,2,3), keepdims=True)
 
     s = 1./s
     self.output *= s
 
-#    value of delta with if truth is None
-    self.delta = np.zeros(shape = self.out_shape())
+    # value of delta if truth is None
+    self.delta = np.zeros(shape=self.out_shape())
 
     if truth is not None:
       out = self.output * (1. / self.output.sum())
       self.cost = -np.sum(truth*np.log(out))
-#      Updateof delta given truth
+      # Update of delta given truth
       self.delta = truth-out
 
   def backward(self, delta=None):
-    """
+    '''
     Backward function of the Softmax Layer.
 
     Parameters:
       delta : array of shape (batch, w, h, c), default is None. If an array is passed,
         it's the global delta to be backpropagated
-    """
+    '''
 
-#    s = (self.output * delta).sum()
-#    delta[:] += self.temperature * self.output * (self.delta - s) #maybe output normalized
+    # s = (self.output * delta).sum()
+    # delta[:] += self.temperature * self.output * (self.delta - s) #maybe output normalized
 
+    # This is an approximation
     if delta is not None:
       delta[:] += self.delta
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
   import os
 
@@ -112,22 +113,16 @@ if __name__ == "__main__":
   groups      = 1
   temperature = 1.
 
-  if spatial:
-    axis = -1
-  else :
-    axis = (1,2,3)
-
   np.random.seed(123)
-#  inpt = np.random.uniform(low = 0., high = 1., size = (2,10,10,3))
-
   batch, w, h, c = inpt.shape
 
-#  Ground truth definition, is random so don't expect much
+  # truth definition, it's random so don't expect much
   truth = np.random.choice([0., 1.], p = [.5,.5], size=(batch,w,h,c))
 
-  layer = Softmax_layer(groups = 1, temperature = 1., spatial = spatial)
+  # Model initialization
+  layer = Softmax_layer(groups=1, temperature=1., spatial=spatial)
 
-#  FORWARD
+  # FORWARD
 
   layer.forward(inpt, truth)
   forward_out = layer.output
@@ -136,28 +131,30 @@ if __name__ == "__main__":
   print(layer)
   print('Loss: {:.3f}'.format(layer_loss))
 
-#  BACKWARD
+  # BACKWARD
 
   delta = np.zeros(shape = inpt.shape)
   layer.backward(delta)
 
-  #Visualizations
+  # Visualizations
 
   fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3, figsize=(10,5))
   fig.subplots_adjust(left=0.1, right=0.95, top=0.95, bottom=0.15)
-  fig.suptitle("SoftMax Layer \n\n loss : {:3.1f}, \n spatial : {}, temperature : {}, groups : {}".format(layer_loss, spatial, temperature, groups))
+  fig.suptitle(('SoftMax Layer \n\n' +
+               'loss : {:.3f}, \n' +
+               'spatial : {}, temperature : {}, groups : {}').format(layer_loss, spatial, temperature, groups))
 
   ax1.imshow(float_2_img(inpt[0]))
-  ax1.set_title("Original Image")
-  ax1.axis("off")
+  ax1.set_title('Original Image')
+  ax1.axis('off')
 
   ax2.imshow(float_2_img(forward_out[0]))
-  ax2.set_title("Forward")
-  ax2.axis("off")
+  ax2.set_title('Forward')
+  ax2.axis('off')
 
   ax3.imshow(float_2_img(delta[0]))
-  ax3.set_title("Backward")
-  ax3.axis("off")
+  ax3.set_title('Backward')
+  ax3.axis('off')
 
   plt.show()
 
