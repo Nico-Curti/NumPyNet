1fd337c43c0d2865763486242ea26e865542319a
Mattia <mattia.ceccarelli3@gmail.com>
2019-10-27 16:42:25 +0100

    converted MaxPool2d to tf.nn.max_pool2d, should be fixed now

--- testing/test_maxpool_layer.py
+++ testing/test_maxpool_layer.py
@@ -4,12 +4,10 @@
 from __future__ import division
 from __future__ import print_function
 
-from keras.models import Model
-from keras.layers import Input
 import keras.backend as K
+import tensorflow as tf
 
 from NumPyNet.layers.maxpool_layer import Maxpool_layer
-from keras.layers import MaxPool2D
 
 import numpy as np
 from hypothesis import strategies as st
@@ -31,28 +29,33 @@ __package__ = 'MaxPool Layer testing'
 def test_maxpool_layer(batch, w, h, c, size, stride, pad):
   '''
   Tests:
-    if the numpy_net maxpool layer forward is consistent with Keras
-    if the numpy_net maxpool layer backward is the same as Keras
-    for differentsizes and strides
-  to be:
+    if the NumPyNet maxpool layer forward is consistent with Keras
+    if the NumPyNet maxpool layer backward is the same as Keras
+      
+    both for different sizes, strides and padding values
+    
+  TODO:
   '''
+
   inpt = np.random.uniform(low=0., high=1., size=(batch, w, h, c))
+
+  # tensor value of inpt, used to computes gradients 
+  inpt_tf = tf.convert_to_tensor(inpt) 
 
   # Numpy_net model
   numpynet = Maxpool_layer(size=size, stride=stride, padding=pad)
 
   if pad:
-    keras_pad = 'same'
+    keras_pad = 'SAME'
   else :
-    keras_pad = 'valid'
+    keras_pad = 'VALID'
 
-  # Keras model initialization.
-  inp = Input(shape=(w, h, c), batch_shape=inpt.shape)
-  x = MaxPool2D(pool_size=size, strides=stride, padding=keras_pad)(inp)
-  model = Model(inputs=[inp], outputs=x)
+  out_keras = tf.nn.max_pool2d(input=inpt_tf, 
+                               ksize=size, strides=stride, 
+                               padding=keras_pad,
+                               data_format='NHWC')
 
-  # Keras Output
-  forward_out_keras = model.predict(inpt)
+  forward_out_keras = K.eval(out_keras)
 
   # numpynet forward and output
   numpynet.forward(inpt)
@@ -65,10 +68,10 @@ def test_maxpool_layer(batch, w, h, c, size, stride, pad):
   # BACKWARD
 
   # Compute the gradient of output w.r.t input
-  gradient = K.gradients(model.output, [model.input])
+  gradient = tf.gradients(out_keras, [inpt_tf])
 
   # Define a function to evaluate the gradient
-  func = K.function(model.inputs + [model.output], gradient)
+  func = K.function([inpt_tf] + [out_keras], gradient)
 
   # Compute delta for Keras
   delta_keras = func([inpt])[0]
@@ -86,5 +89,9 @@ def test_maxpool_layer(batch, w, h, c, size, stride, pad):
   assert np.allclose(delta, delta_keras, atol=1e-8)
 
 if __name__ == '__main__':
-
   test_maxpool_layer()
+
+  
+  
+  
+  
