4d419ec50aa7725ada8671592f15a5c29fcdaaff
Mat092 <mattia.ceccarelli3@gmail.com>
2020-02-19 21:35:10 +0100

    test avgpool

--- testing/test_avgpool_layer.py
+++ testing/test_avgpool_layer.py
@@ -8,83 +8,185 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input
 import tensorflow.keras.backend as K
 
+from NumPyNet.exception import LayerError
+from NumPyNet.exception import NotFittedError
 from NumPyNet.layers.avgpool_layer import Avgpool_layer
 from tensorflow.keras.layers import AvgPool2D
 
 import numpy as np
+import pytest
 from hypothesis import strategies as st
-from hypothesis import given, settings
+from hypothesis import given
+from hypothesis import settings
 
 __author__ = ['Mattia Ceccarelli', 'Nico Curti']
 __email__ = ['mattia.ceccarelli3@studio.unibo.it', 'nico.curti2@unibo.it']
-__package__ = 'AvgPool Layer testing'
+# __package__ = 'AvgPool Layer testing'
 
-@given(batch  = st.integers(min_value=1, max_value=15),
-       w      = st.integers(min_value=15, max_value=100),
-       h      = st.integers(min_value=15, max_value=100),
-       c      = st.integers(min_value=1, max_value=10),
-       size   = st.integers(min_value=1, max_value=10),
-       stride = st.integers(min_value=1, max_value=10),
-       pad    = st.booleans())
-@settings(max_examples=10,
-          deadline=None)
-def test_avgpool_layer(batch, w, h, c, size, stride, pad):
+
+class TestAvgpoolLayer(object):
   '''
   Tests:
-    if the average pool layer forwards and backward are consistent with keras
-
-  to be:
+    - costructor of Avgpool_layer object
+    - print function
+    - forward function against tf.keras
+    - backward function against tf.keras
   '''
 
-  inpt = np.random.uniform(low=0., high=1., size=(batch, w, h, c))
+  @given(size   = st.integers(min_value=-10, max_value=10),
+         stride = st.integers(min_value=0, max_value=10),
+         pad    = st.booleans())
+  @settings(max_examples=20,
+            deadline=None)
+  def test_costructor (self, size, stride, pad):
 
-  # Numpy_net model
-  numpynet = Avgpool_layer(size=size, stride=stride, padding=pad)
+    if size <= 0 :
 
-  if pad:
-    keras_pad = 'same'
-  else :
-    keras_pad = 'valid'
+      with pytest.raises(LayerError):
+        layer = Avgpool_layer(size=size, stride=stride, padding=pad)
 
-  # Keras model initialization.
-  inp = Input(batch_shape=inpt.shape)
-  x = AvgPool2D(pool_size=size, strides=stride, padding=keras_pad)(inp)
-  model = Model(inputs=[inp], outputs=x)
+    else:
+      layer = Avgpool_layer(size=size, stride=stride, padding=pad)
 
-  # Keras Output
-  forward_out_keras = model.predict(inpt)
+      assert layer.size        == (size, size)
+      assert len(layer.size)   == 2
 
-  # numpynet forward and output
-  numpynet.forward(inpt)
-  forward_out_numpynet = numpynet.output
+      if stride:
+        assert layer.stride == (stride, stride)
+      else:
+        assert layer.size == layer.stride
 
-  # Test for dimension and allclose of all output
-  assert forward_out_numpynet.shape == forward_out_keras.shape
-  assert np.allclose(forward_out_numpynet, forward_out_keras, atol=1e-6)
+      assert len(layer.stride) == 2
 
-  # BACKWARD
+      assert layer.delta   == None
+      assert layer.output  == None
 
-  # Compute the gradient of output w.r.t input
-  gradient = K.gradients(model.output, [model.input])
+      assert layer.batch == 0
+      assert layer.w == 0
+      assert layer.h == 0
+      assert layer.c == 0
 
-  # Define a function to evaluate the gradient
-  func = K.function(model.inputs + [model.output], gradient)
+      assert layer.pad        == pad
+      assert layer.pad_left   == 0
+      assert layer.pad_right  == 0
+      assert layer.pad_top    == 0
+      assert layer.pad_bottom == 0
 
-  # Compute delta for Keras
-  delta_keras = func([inpt])[0]
 
-  # Definition of starting delta for numpynet
-  numpynet.delta = np.ones(shape=numpynet.out_shape, dtype=float)
-  delta = np.zeros(shape=inpt.shape, dtype=float)
+  @given(size   = st.integers(min_value=1, max_value=10),
+         stride = st.integers(min_value=0, max_value=10),
+         pad    = st.booleans())
+  @settings(max_examples=10,
+            deadline=None)
+  def test_printer (self, size, stride, pad):
 
-  # numpynet Backward
-  numpynet.backward(delta)
+    layer = Avgpool_layer(size=size, stride=stride, padding=pad)
 
-  # Back tests
-  assert delta.shape == delta_keras.shape
-  assert delta.shape == inpt.shape
-  assert np.allclose(delta, delta_keras, atol=1e-8)
+    #TODO : to be discussed
+    print(layer)
 
-if __name__ == '__main__':
+  @given(batch  = st.integers(min_value=1, max_value=15),
+         w      = st.integers(min_value=15, max_value=100),
+         h      = st.integers(min_value=15, max_value=100),
+         c      = st.integers(min_value=1, max_value=10),
+         size   = st.integers(min_value=1, max_value=10),
+         stride = st.integers(min_value=1, max_value=10),
+         pad    = st.booleans())
+  @settings(max_examples=10,
+            deadline=None)
+  def test_forward (self, batch, w, h, c, size, stride, pad):
 
-  test_avgpool_layer()
+    inpt = np.random.uniform(low=0., high=1., size=(batch, w, h, c))
+
+    # Numpy_net model
+    numpynet = Avgpool_layer(size=size, stride=stride, padding=pad)
+
+    if pad:
+      keras_pad = 'same'
+    else :
+      keras_pad = 'valid'
+
+    # Keras model initialization.
+    inp   = Input(batch_shape=inpt.shape)
+    x     = AvgPool2D(pool_size=size, strides=stride, padding=keras_pad)(inp)
+    model = Model(inputs=[inp], outputs=x)
+
+    # Keras Output
+    forward_out_keras = model.predict(inpt)
+
+    # numpynet forward and output
+    numpynet.forward(inpt)
+    forward_out_numpynet = numpynet.output
+
+    # Test for dimension and allclose of all output
+    assert forward_out_numpynet.shape == forward_out_keras.shape
+    assert np.allclose(forward_out_numpynet, forward_out_keras, atol=1e-8)
+
+
+  @given(batch  = st.integers(min_value=1, max_value=15),
+         w      = st.integers(min_value=15, max_value=100),
+         h      = st.integers(min_value=15, max_value=100),
+         c      = st.integers(min_value=1, max_value=10),
+         size   = st.integers(min_value=1, max_value=10),
+         stride = st.integers(min_value=1, max_value=10),
+         pad    = st.booleans())
+  @settings(max_examples=10,
+            deadline=None)
+  def test_backward (self, batch, w, h, c, size, stride, pad):
+
+    inpt = np.random.uniform(low=0., high=1., size=(batch, w, h, c))
+
+    # Numpy_net model
+    numpynet = Avgpool_layer(size=size, stride=stride, padding=pad)
+
+    if pad:
+      keras_pad = 'same'
+    else :
+      keras_pad = 'valid'
+
+    # Keras model initialization.
+    inp   = Input(batch_shape=inpt.shape)
+    x     = AvgPool2D(pool_size=size, strides=stride, padding=keras_pad)(inp)
+    model = Model(inputs=[inp], outputs=x)
+
+    # Keras Output
+    forward_out_keras = model.predict(inpt)
+
+    # try to backward
+    with pytest.raises(NotFittedError):
+      # Global delta init.
+      delta = np.empty(shape=inpt.shape, dtype=float)
+
+      # numpynet Backward
+      numpynet.backward(delta)
+
+    # numpynet forward and output
+    numpynet.forward(inpt)
+    forward_out_numpynet = numpynet.output
+
+    # Test for dimension and allclose of all output
+    assert forward_out_numpynet.shape == forward_out_keras.shape
+    assert np.allclose(forward_out_numpynet, forward_out_keras, atol=1e-8)
+
+    # BACKWARD
+
+    # Compute the gradient of output w.r.t input
+    gradient = K.gradients(model.output, [model.input])
+
+    # Define a function to evaluate the gradient
+    func = K.function(model.inputs + [model.output], gradient)
+
+    # Compute delta for Keras
+    delta_keras = func([inpt])[0]
+
+    # Definition of starting delta for numpynet
+    numpynet.delta = np.ones(shape=numpynet.out_shape, dtype=float)
+    delta = np.zeros(shape=inpt.shape, dtype=float)
+
+    # numpynet Backward
+    numpynet.backward(delta)
+
+    # Back tests
+    assert delta.shape == delta_keras.shape
+    assert delta.shape == inpt.shape
+    assert np.allclose(delta, delta_keras, atol=1e-8)
